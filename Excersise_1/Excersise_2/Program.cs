8ec64836860a2621dd34eff5a58f9cbf24cb25c6
Алексей Мещанов <meschanovim@mail.ru>
2023-01-19 20:55:32 +0300

    Добавил решение к задаче 2 первого семинара

--- Excersise_1/Excersise_2/Program.cs
+++ Excersise_1/Excersise_2/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿Console.WriteLine
+("Представлена программа, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее");
+Console.Write("Введите число a:");
+int numbera=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b:");
+int numberb=Convert.ToInt32(Console.ReadLine());
+if(numbera>numberb) 
+{
+    Console.Write("Число "+numbera+" большее, число "+numberb+" меньшее.");
+
+}
+
+else if (numbera==numberb)
+{
+    Console.Write("Число "+numbera+" равняется числу "+numberb+".");
+}
+
+else
+{
+    Console.Write("Число "+numberb+" большее, число "+numbera+ " меньшее.");
+}
+
+ 
