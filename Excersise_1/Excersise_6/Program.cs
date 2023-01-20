d9fb4757c0d4564bf8b563e67d5d9b3ee69f8bdd
Алексей Мещанов <meschanovim@mail.ru>
2023-01-20 09:49:48 +0300

    Добавили решение Excersise_6

---
+++ Excersise_1/Excersise_6/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine
+("Программа, которая на вход принимает число и выдаёт, является ли число чётным");
+Console.Write("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number%2==0)
+
+{
+    Console.Write(number+" - число четное");
+}
+
+else 
+
+{
+Console.Write(number+" - число не четное");
+}
+
