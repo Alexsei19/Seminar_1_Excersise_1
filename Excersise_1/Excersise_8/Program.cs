353c72888d0c1ac9667bcd766127f42db0dfa526
Алексей Мещанов <meschanovim@mail.ru>
2023-01-20 14:13:31 +0300

    Добавил решение к задаче 8 семинара 1

---
+++ Excersise_1/Excersise_8/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine
+("Программа, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N");
+Console.Write("введите  число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while (i <= n)
+
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + ",");
+    }
+    
+    i = i + 1;
+}
+
