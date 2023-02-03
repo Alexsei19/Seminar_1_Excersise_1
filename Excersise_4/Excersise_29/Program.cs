a420dccdeb7e8dce8e967058f0c244d38841c3f6
Алексей Мещанов <meschanovim@mail.ru>
2023-02-03 07:51:25 +0300

    Добавлено решение задач Excersise_4

---
+++ Excersise_4/Excersise_29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, -5, 0, -11 -> [1, 2, 5, 7, 19, -5, 0, -11]
+
+Console.Clear();
+Console.WriteLine("Программа, которая задаёт массив из 8 элементов и выводит их на экран.");
+
+int [] array = new int [8];
+
+for (int i=0; i<array.Length;i++)
+{
+Console.Write ($"Задай элемент №{i+1}:    ");
+array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine($"Массив[{string.Join(", ", array)}]");
